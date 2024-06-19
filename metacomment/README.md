f1684e3142f99f6af70a92d99dfbe745f9031c31
winter.loo <adjkldd@126.com>
2024-06-19 15:07:23 +0800

    extract independent multi-line c code comments

---
+++ metacomment/README.md
@@ -0,0 +1,11 @@
+#metacomment
+
+Extract block comments from C code. It
+
+* does not extract inline comments
+* does not extract single line comments
+* extracts multi-line comments
+
+```
+cargo run tests/brin.c
+```
--- metacomment/src/main.rs
+++ metacomment/src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 use std::fmt::Display;
-use std::io::{self, Read, Seek, SeekFrom, Write};
+#[cfg(env = "OUTPUT_ORIGIN")]
+use std::io::Write;
+use std::io::{self, Read, Seek, SeekFrom};
+
+mod comment_of_c;
+use comment_of_c::*;
 
 struct CommentOfC {
     offset: usize,
@@ -24,13 +29,6 @@ impl CommentOfC {
             content: String::from(""),
             pre_whitespaces: String::from(""),
         }
-    }
-
-    fn reset(&mut self) {
-        self.offset = 0;
-        self.end = 0;
-        self.content.clear();
-        self.pre_whitespaces.clear();
     }
 }
 
@@ -61,115 +59,37 @@ impl CommentExtractor {
 
     fn buffer_read(&mut self) {
         let unconsumed = self.end - self.offset;
-        println!("-- unconsumed {}", unconsumed);
         self.buffer.copy_within(self.offset..self.end, 0);
         self.file_offset += self.offset;
         self.offset = 0;
         self.end = unconsumed;
 
         let len = self.code_file.read(&mut self.buffer[unconsumed..]).unwrap();
-        println!("buffer_read len: {}", len);
         self.end += len;
 
-        let mut file = std::fs::OpenOptions::new()
-            .append(true) // Open in append mode
-            .create(true) // Create the file if it doesn't exist
-            .open("out.c").expect("out.c");
-        file.write_all(&self.buffer).expect("write all");
-
+        #[cfg(env = "OUTPUT_ORIGIN")]
+        {
+            let mut file = std::fs::OpenOptions::new()
+                .append(true) // Open in append mode
+                .create(true) // Create the file if it doesn't exist
+                .open("out.c")
+                .expect("out.c");
+            file.write_all(&self.buffer).expect("write all");
+        }
     }
 
-    fn find_comment(&mut self) -> Option<CommentOfC> {
-        let mut com = CommentOfC::new();
-        let mut found_comment = false;
-        let mut i = self.offset;
-        let mut maybe_inline_comment = false;
-        let mut pre_whitespaces = String::from("");
-
-        while i < self.end {
-            if i + 2 < self.end
-                && (self.buffer[i] != b'\'' && self.buffer[i] != b'"')
-                && self.buffer[i + 1] == b'/'
-                && self.buffer[i + 2] == b'*' || (self.file_offset == 0 && i == 0 && i + 1 < self.end && self.buffer[i] == b'/' && self.buffer[i + 1] == b'*')
-            {
-                if self.buffer[i] == b'/' {
-                    com.offset = self.file_offset + i;
-                } else {
-                    com.offset = self.file_offset + i + 1;
-                }
-                com.content.push('/');
-                com.content.push('*');
-
-                if self.buffer[i] == b' ' || self.buffer[i] == b'\t' {
-                    pre_whitespaces.push(self.buffer[i] as char);
-                }
-
-                if self.buffer[i] != b'/' {
-                    i += 1
-                }
-
-                if !maybe_inline_comment {
-                    com.pre_whitespaces = pre_whitespaces.clone();
-                } else {
-                    // we do not process inline comments
-                    i += 2;
-                    continue;
-                }
-
-                let mut multiline = false;
-
-                i += 2; // Skip past the '/*'
-                while i + 1 < self.end {
-                    if self.buffer[i] == b'*' && self.buffer[i + 1] == b'/' {
-                        com.end = self.file_offset + i + 2;
-                        if multiline {
-                            found_comment = true;
-                            com.content.push('*');
-                            com.content.push('/');
-
-                            pre_whitespaces.clear();
-                            break;
-                        } else {
-                            com.reset();
-                            break;
-                        }
-                    } else {
-                        if self.buffer[i] == b'\n'
-                            || self.buffer[i] == b'\r'
-                            || (i + 1 < self.end
-                                && self.buffer[i] == b'\r'
-                                && self.buffer[i] == b'\n')
-                        {
-                            multiline = true;
-                        }
-                        com.content.push(self.buffer[i] as char);
-                        i += 1;
-                    }
-                }
-            }
-            if self.buffer[i] == b'\n'
-                || self.buffer[i] == b'\r'
-                || (i + 1 < self.end && self.buffer[i] == b'\r' && self.buffer[i] == b'\n')
-            {
-                maybe_inline_comment = false;
-                pre_whitespaces.clear();
-            } else if self.buffer[i] != b' ' && self.buffer[i] != b'\t' {
-                maybe_inline_comment = true;
-            } else {
-                maybe_inline_comment = false;
-                pre_whitespaces.push(self.buffer[i] as char);
-            }
-            if found_comment {
-                break;
-            }
-            i += 1;
-        }
-
-        if found_comment {
-            self.offset = com.end + 1;
+    fn find_first_comment(&mut self) -> Option<CommentOfC> {
+        let pos = find_first_c_comment(&self.buffer[self.offset..self.end]);
+        if let Some((start, end)) = pos {
+            let mut com = CommentOfC::new();
+            com.offset = self.file_offset + self.offset + start;
+            com.end = self.file_offset + self.offset + end;
+            com.content.push_str(&String::from_utf8_lossy(
+                &self.buffer[(self.offset + start)..(self.offset + end)],
+            ));
+            self.offset += end;
             Some(com)
         } else {
-            self.offset = self.end;
             None
         }
     }
@@ -179,11 +99,11 @@ impl Iterator for CommentExtractor {
     type Item = CommentOfC;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(com) = self.find_comment() {
+        if let Some(com) = self.find_first_comment() {
             Some(com)
         } else {
             self.buffer_read();
-            self.find_comment()
+            self.find_first_comment()
         }
     }
 }
@@ -200,5 +120,6 @@ fn main() -> io::Result<()> {
     for com in ce {
         println!("{}", com);
     }
+
     Ok(())
 }
