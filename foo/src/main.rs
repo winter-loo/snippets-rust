d32dc2bde89ab166b7e963b18398bce858a51c07
winter.loo <adjkldd@126.com>
2025-03-24 09:09:19 +0800

    update foo

--- foo/src/main.rs
+++ foo/src/main.rs
@@ -1,49 +1,173 @@
 fn main() {
-    let a = 512;
-    let b: Vec<u8> = format!("{a:b}").into_bytes().iter().map(|u| u - '0' as u8).collect();
-    println!("{b:?}");
-    let v: Vec<_> = (0..(7 + b.len()/7) / 7).map(|i| {
-        let end = b.len() - i * 7;
-        let start = end.saturating_sub(7);
-        &b[start..end]
-    }).collect();
-    println!("{v:?}");
+    let mut f = Forth::new();
+    assert!(f.eval(": foo 5 ;").is_ok());
+    assert!(f.eval(": bar foo ;").is_ok());
+    assert!(f.eval(": foo 6 ;").is_ok());
+    assert!(f.eval("bar foo").is_ok());
+    assert_eq!(f.stack(), [5, 6]);
 
+    let mut f = Forth::new();
+    assert!(f.eval(": foo 10 ;").is_ok());
+    assert!(f.eval(": foo foo 1 + ;").is_ok());
+    assert!(f.eval(": foo foo 1 + ;").is_ok());
+    assert!(f.eval("foo").is_ok());
 
-    let input = &[268_435_455];
-    let output = to_bytes(input);
-    let expected = vec![0xff, 0xff, 0xff, 0x7f];
-    // assert_eq!(output, expected);
+    let mut f = Forth::new();
+    f.eval(": a 0 drop ;").unwrap();
+    f.eval(": b a a ;").unwrap();
+    f.eval(": c b b ;").unwrap();
+    f.eval(": d c c ;").unwrap();
+    f.eval(": e d d ;").unwrap();
 
-    let input = &[134_217_728];
-    let output = to_bytes(input);
-    let expected = vec![0xc0, 0x80, 0x80, 0x0];
-    // assert_eq!(output, expected);
+    assert!(f.stack().is_empty());
+}
+
+use std::collections::{HashMap, VecDeque};
+
+pub type Value = i32;
+pub type Result = std::result::Result<(), Error>;
+
+pub struct Forth {
+    defs: HashMap<String, String>,
+    stack: Vec<Value>,
+}
+
+struct Def {
+    name: String,
+    namespace: HashMap<String, u32>,
+    value: String,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
-    IncompleteNumber,
+    DivisionByZero,
+    StackUnderflow,
+    UnknownWord,
+    InvalidWord,
 }
 
-/// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
-pub fn to_bytes(values: &[u32]) -> Vec<u8> {
-    for number in values {
-        let s: Vec<u8> = format!("{number:b}").into_bytes().iter().map(|n| n - '0' as u8).collect();
-        // (0..=(s.len() + 7) / 7).map(move |i| s[i..i+7].to_vec());
-        // }).map(|mut s| {
-        //     s.get_mut(0).map(|b| *b = 1);
-        //         s
-        //     }).collect();
-        // v.last_mut().map(|u| {
-        //     u.get_mut(0).map(|b| *b = 0);
-        //     u
-        // });
+impl Forth {
+    pub fn new() -> Forth {
+        Forth {
+            defs: HashMap::new(),
+            stack: vec![],
+        }
     }
-    vec![]
-}
 
-/// Given a stream of bytes, extract all numbers which are encoded in there.
-pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
-    todo!("Convert the list of bytes {bytes:?} to a list of numbers")
+    pub fn stack(&self) -> &[Value] {
+        &self.stack
+    }
+
+    fn get_number(&mut self) -> std::result::Result<Value, Error> {
+        match self.stack.pop() {
+            None => Err(Error::StackUnderflow),
+            Some(x) => Ok(x),
+        }
+    }
+
+    fn rewrite_def(&self, def: &Vec<String>) -> String {
+        let mut newdef = Vec::with_capacity(def.len());
+        for token in def {
+            if let Some(odef) = self.defs.get(token) {
+                newdef.push(odef.to_string());
+            } else {
+                newdef.push(token.to_string());
+            }
+        }
+        newdef.join(" ")
+    }
+
+    // 1 2 +
+    pub fn eval(&mut self, input: &str) -> Result {
+        let mut expr: VecDeque<String> = input.split(' ').map(|s| s.to_ascii_lowercase()).collect();
+        // : FOO 1 2 3 ;
+        if let Some(first) = expr.front() {
+            if first == ":" {
+                if expr.len() < 4 {
+                    return Err(Error::InvalidWord);
+                }
+                if let Some(last) = expr.back() {
+                    if last != ";" {
+                        return Err(Error::InvalidWord);
+                    }
+                    expr.pop_back();
+                    expr.pop_front();
+                    let identifier = expr[0].to_string().to_ascii_lowercase();
+                    if identifier.parse::<Value>().is_ok() {
+                        return Err(Error::InvalidWord);
+                    }
+                    expr.pop_front();
+                    let def = expr.into_iter().collect::<Vec<_>>();
+                    let def = self.rewrite_def(&def);
+                    self.defs.insert(identifier, def);
+                    return Ok(());
+                } else {
+                    return Err(Error::InvalidWord);
+                }
+            }
+        }
+        while let Some(token) = expr.pop_front() {
+            if let Ok(n) = token.parse::<Value>() {
+                self.stack.push(n);
+                continue;
+            }
+            if let Some(x) = self.defs.get(&token) {
+                expr.extend(x.split(' ').map(|s| s.to_string()));
+            } else {
+                match &token[..] {
+                    "+" => {
+                        let op1 = self.get_number()?;
+                        let op2 = self.get_number()?;
+                        self.stack.push(op2 + op1);
+                    }
+                    "-" => {
+                        let op1 = self.get_number()?;
+                        let op2 = self.get_number()?;
+                        self.stack.push(op2 - op1);
+                    }
+                    "*" => {
+                        let op1 = self.get_number()?;
+                        let op2 = self.get_number()?;
+                        self.stack.push(op2 * op1);
+                    }
+                    "/" => {
+                        let op1 = self.get_number()?;
+                        let op2 = self.get_number()?;
+                        if op1 == 0 {
+                            return Err(Error::DivisionByZero);
+                        }
+                        self.stack.push(op2 / op1);
+                    }
+                    "dup" => {
+                        let last = match self.stack.last() {
+                            None => return Err(Error::StackUnderflow),
+                            Some(x) => *x,
+                        };
+                        self.stack.push(last);
+                    }
+                    "drop" => {
+                        if self.stack.pop().is_none() {
+                            return Err(Error::StackUnderflow);
+                        }
+                    }
+                    "swap" => {
+                        let op1 = self.get_number()?;
+                        let op2 = self.get_number()?;
+                        self.stack.push(op1);
+                        self.stack.push(op2);
+                    }
+                    "over" => {
+                        let op1 = match self.stack.iter().nth_back(1) {
+                            None => return Err(Error::StackUnderflow),
+                            Some(x) => *x,
+                        };
+
+                        self.stack.push(op1);
+                    }
+                    _ => return Err(Error::UnknownWord),
+                }
+            }
+        }
+        Ok(())
+    }
 }
