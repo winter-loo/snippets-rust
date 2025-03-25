cd7c9f4a4999c86b07051eeab3e06787a56a8281
winter.loo <adjkldd@126.com>
2025-03-25 20:28:37 +0800

    update foo

--- foo/src/main.rs
+++ foo/src/main.rs
@@ -1,264 +1,231 @@
 fn main() {
-    FONTS.iter().for_each(|font| {
-        let v: Vec<_> = font.1.iter().map(|v| std::string::String::from_utf8(v.to_vec()).unwrap()).collect();
-        let v = v.join("\n");
-        println!("{v}");
-    });
+
 }
 
-mod tests {
-    use super::*;
-    #[test]
-    fn input_with_lines_not_multiple_of_four_is_error() {
-        #[rustfmt::skip]
-    let input = " _ \n".to_string() +
-                "| |\n" +
-                "   ";
-        assert_eq!(Err(Error::InvalidRowCount(3)), convert(&input));
-    }
-    #[test]
-    fn input_with_columns_not_multiple_of_three_is_error() {
-        #[rustfmt::skip]
-    let input = "    \n".to_string() +
-                "   |\n" +
-                "   |\n" +
-                "    ";
-        assert_eq!(Err(Error::InvalidColumnCount(4)), convert(&input));
-    }
-    #[test]
-    fn unrecognized_characters_return_question_mark() {
-        #[rustfmt::skip]
-    let input = "   \n".to_string() +
-                "  _\n" +
-                "  |\n" +
-                "   ";
-        assert_eq!(Ok("?".to_string()), convert(&input));
-    }
-    #[test]
-    fn recognizes_0() {
-        #[rustfmt::skip]
-    let input = " _ \n".to_string() +
-                "| |\n" +
-                "|_|\n" +
-                "   ";
-        assert_eq!(Ok("0".to_string()), convert(&input));
-    }
-    #[test]
-    fn recognizes_1() {
-        #[rustfmt::skip]
-    let input = "   \n".to_string() +
-                "  |\n" +
-                "  |\n" +
-                "   ";
-        assert_eq!(Ok("1".to_string()), convert(&input));
-    }
-    #[test]
-    fn recognizes_2() {
-        #[rustfmt::skip]
-    let input = " _ \n".to_string() +
-                " _|\n" +
-                "|_ \n" +
-                "   ";
-        assert_eq!(Ok("2".to_string()), convert(&input));
-    }
-    #[test]
-    fn recognizes_3() {
-        #[rustfmt::skip]
-    let input = " _ \n".to_string() +
-                " _|\n" +
-                " _|\n" +
-                "   ";
-        assert_eq!(Ok("3".to_string()), convert(&input));
-    }
-    #[test]
-    fn recognizes_4() {
-        #[rustfmt::skip]
-    let input = "   \n".to_string() +
-                "|_|\n" +
-                "  |\n" +
-                "   ";
-        assert_eq!(Ok("4".to_string()), convert(&input));
-    }
-    #[test]
-    fn recognizes_5() {
-        #[rustfmt::skip]
-    let input = " _ \n".to_string() +
-                "|_ \n" +
-                " _|\n" +
-                "   ";
-        assert_eq!(Ok("5".to_string()), convert(&input));
-    }
-    #[test]
-    fn recognizes_6() {
-        #[rustfmt::skip]
-    let input = " _ \n".to_string() +
-                "|_ \n" +
-                "|_|\n" +
-                "   ";
-        assert_eq!(Ok("6".to_string()), convert(&input));
-    }
-    #[test]
-    fn recognizes_7() {
-        #[rustfmt::skip]
-    let input = " _ \n".to_string() +
-                "  |\n" +
-                "  |\n" +
-                "   ";
-        assert_eq!(Ok("7".to_string()), convert(&input));
-    }
-    #[test]
-    fn recognizes_8() {
-        #[rustfmt::skip]
-    let input = " _ \n".to_string() +
-                "|_|\n" +
-                "|_|\n" +
-                "   ";
-        assert_eq!(Ok("8".to_string()), convert(&input));
-    }
-    #[test]
-    fn recognizes_9() {
-        #[rustfmt::skip]
-    let input = " _ \n".to_string() +
-                "|_|\n" +
-                " _|\n" +
-                "   ";
-        assert_eq!(Ok("9".to_string()), convert(&input));
-    }
-}
+use std::collections::HashMap;
 
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
+/// `InputCellId` is a unique identifier for an input cell.
+#[derive(Clone, Hash, Copy, Debug, PartialEq, Eq)]
+pub struct InputCellId(usize);
+/// `ComputeCellId` is a unique identifier for a compute cell.
+/// Values of type `InputCellId` and `ComputeCellId` should not be mutually assignable,
+/// demonstrated by the following tests:
+///
+/// ```compile_fail
+/// let mut r = react::Reactor::new();
+/// let input: react::ComputeCellId = r.create_input(111);
+/// ```
+///
+/// ```compile_fail
+/// let mut r = react::Reactor::new();
+/// let input = r.create_input(111);
+/// let compute: react::InputCellId = r.create_compute(&[react::CellId::Input(input)], |_| 222).unwrap();
+/// ```
+#[derive(Clone, Hash, Copy, Debug, PartialEq, Eq)]
+pub struct ComputeCellId(usize);
+#[derive(Hash, Clone, Copy, Debug, PartialEq, Eq)]
+pub struct CallbackId(usize);
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum CellId {
+    Input(InputCellId),
+    Compute(ComputeCellId),
+}
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum Error {
-    InvalidRowCount(usize),
-    InvalidColumnCount(usize),
+pub enum RemoveCallbackError {
+    NonexistentCell,
+    NonexistentCallback,
 }
 
-//    _  _
-//   | _| _|
-//   ||_  _|
-// 
-//     _  _
-// |_||_ |_
-//   | _||_|
-// 
-//  _  _  _
-//   ||_||_|
-//   ||_| _|
-// 
-pub fn convert(input: &str) -> Result<String, Error> {
-    // let matrix: Vec<_> = input
-    //     .lines()
-    //     .map(|line| line.bytes().collect::<Vec<_>>())
-    //     .collect();
-    let mut matrix = vec![vec![]; 4];
-    for (i, line) in input.lines().enumerate() {
-        let bytes: Vec<_> = line.bytes().collect();
-        let chunks: Vec<_> = bytes.chunks(3).collect();
-        if chunks.last().is_some_and(|c| c.len() != 3) {
-            return Err(Error::InvalidColumnCount(2));
+pub struct Reactor<'a, T> {
+    // Just so that the compiler doesn't complain about an unused type parameter.
+    // You probably want to delete this field.
+    input_cells: HashMap<InputCellId, (T, Vec<ComputeCellId>)>,
+    compute_cells: HashMap<
+        ComputeCellId,
+        (
+            Vec<CellId>,
+            Box<dyn Fn(&[T]) -> T>,
+            Vec<CallbackId>,
+            Vec<ComputeCellId>,
+        ),
+    >,
+    num_cells: usize,
+    // lifetime `'a` ensures that the closures remain valid for the lifetime of the Reactor
+    // instance
+    callbacks: HashMap<CallbackId, Box<dyn 'a + FnMut(T)>>,
+    callback_id_gen: usize,
+}
+
+// You are guaranteed that Reactor will only be tested against types that are Copy + PartialEq.
+impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
+    pub fn new() -> Self {
+        Reactor {
+            input_cells: HashMap::new(),
+            compute_cells: HashMap::new(),
+            num_cells: 0,
+            callbacks: HashMap::new(),
+            callback_id_gen: 0,
         }
-
-        matrix[i].push(chunks);
     }
 
-    if matrix.len() != 4 {
-        return Err(Error::InvalidRowCount(matrix.len()));
+    // Creates an input cell with the specified initial value, returning its ID.
+    pub fn create_input(&mut self, initial: T) -> InputCellId {
+        let id = InputCellId(self.num_cells);
+        self.num_cells += 1;
+        self.input_cells.insert(id, (initial, vec![]));
+        id
     }
-    if !matrix.iter().all(|v| v.len() == 3) {
-        let line = matrix.iter().skip_while(|v| v.len() == 3).next().unwrap();
-        return Err(Error::InvalidColumnCount(line.len()));
+
+    // Creates a compute cell with the specified dependencies and compute function.
+    // The compute function is expected to take in its arguments in the same order as specified in
+    // `dependencies`.
+    // You do not need to reject compute functions that expect more arguments than there are
+    // dependencies (how would you check for this, anyway?).
+    //
+    // If any dependency doesn't exist, returns an Err with that nonexistent dependency.
+    // (If multiple dependencies do not exist, exactly which one is returned is not defined and
+    // will not be tested)
+    //
+    // Notice that there is no way to *remove* a cell.
+    // This means that you may assume, without checking, that if the dependencies exist at creation
+    // time they will continue to exist as long as the Reactor exists.
+    pub fn create_compute<F: Fn(&[T]) -> T + 'static>(
+        &mut self,
+        dependencies: &[CellId],
+        compute_func: F,
+    ) -> Result<ComputeCellId, CellId> {
+        let id = ComputeCellId(self.num_cells);
+
+        for dep in dependencies {
+            match dep {
+                CellId::Input(i) => match self.input_cells.get_mut(i) {
+                    Some((_, dependents)) => {
+                        dependents.push(id);
+                    }
+                    None => return Err(*dep),
+                },
+                CellId::Compute(c) => match self.compute_cells.get_mut(c) {
+                    None => return Err(*dep),
+                    Some((_, _, _, dependents)) => {
+                        dependents.push(id);
+                    }
+                },
+            }
+        }
+        self.num_cells += 1;
+        self.compute_cells.insert(
+            id,
+            (
+                dependencies.to_vec(),
+                Box::new(compute_func),
+                vec![],
+                vec![],
+            ),
+        );
+        Ok(id)
     }
-    Ok(FONTS
-        .iter()
-        .filter(|font| matrix == font.1)
-        .next()
-        .map(|font| font.0.to_string())
-        .unwrap_or("?".to_string()))
+
+    // Retrieves the current value of the cell, or None if the cell does not exist.
+    //
+    // You may wonder whether it is possible to implement `get(&self, id: CellId) -> Option<&Cell>`
+    // and have a `value(&self)` method on `Cell`.
+    //
+    // It turns out this introduces a significant amount of extra complexity to this exercise.
+    // We chose not to cover this here, since this exercise is probably enough work as-is.
+    pub fn value(&self, id: CellId) -> Option<T> {
+        match id {
+            CellId::Input(i) => self.input_cells.get(&i).map(|(t, _)| *t),
+            CellId::Compute(c) => self
+                .compute_cells
+                .get(&c)
+                .map(|(deps, compute_func, _, _)| {
+                    let deps: Option<Vec<_>> = deps.iter().map(|id| self.value(*id)).collect();
+                    deps.map(|v| (compute_func)(&v))
+                })
+                .flatten(),
+        }
+    }
+
+    // Sets the value of the specified input cell.
+    //
+    // Returns false if the cell does not exist.
+    //
+    // Similarly, you may wonder about `get_mut(&mut self, id: CellId) -> Option<&mut Cell>`, with
+    // a `set_value(&mut self, new_value: T)` method on `Cell`.
+    //
+    // As before, that turned out to add too much extra complexity.
+    pub fn set_value(&mut self, id: InputCellId, new_value: T) -> bool {
+        self.input_cells
+            .get_mut(&id)
+            .map_or(false, |(value, dependents)| {
+                if *value != new_value {
+                    *value = new_value;
+                    dependents.iter().for_each(|cid| {
+                        self.compute_cells.get(cid).map(|(deps, compute_func, callbacks, _)| {
+                            callbacks.iter().for_each(|cb_id| {
+                                self.callbacks.get_mut(cb_id).map(|cb_action| {
+                                    let deps: Option<Vec<_>> = deps.iter().map(|id| self.value(*id)).collect();
+                                    let new_compute_value = deps.map(|v| (compute_func)(&v));
+                                    (cb_action)(new_compute_value.unwrap());
+                                });
+                            });
+                        });
+                    });
+                }
+                true
+            })
+    }
+
+    // Adds a callback to the specified compute cell.
+    //
+    // Returns the ID of the just-added callback, or None if the cell doesn't exist.
+    //
+    // Callbacks on input cells will not be tested.
+    //
+    // The semantics of callbacks (as will be tested):
+    // For a single set_value call, each compute cell's callbacks should each be called:
+    // * Zero times if the compute cell's value did not change as a result of the set_value call.
+    // * Exactly once if the compute cell's value changed as a result of the set_value call.
+    //   The value passed to the callback should be the final value of the compute cell after the
+    //   set_value call.
+    pub fn add_callback<F: FnMut(T) + 'a>(
+        &mut self,
+        id: ComputeCellId,
+        callback: F,
+    ) -> Option<CallbackId> {
+        self.compute_cells.get_mut(&id).map(|(_, _, callbacks, _)| {
+            let id = CallbackId(self.callback_id_gen);
+            self.callback_id_gen += 1;
+            self.callbacks.insert(id, Box::new(callback));
+            callbacks.push(id);
+            id
+        })
+    }
+
+    // Removes the specified callback, using an ID returned from add_callback.
+    //
+    // Returns an Err if either the cell or callback does not exist.
+    //
+    // A removed callback should no longer be called.
+    pub fn remove_callback(
+        &mut self,
+        cell: ComputeCellId,
+        callback: CallbackId,
+    ) -> Result<(), RemoveCallbackError> {
+        match self.compute_cells.get_mut(&cell) {
+            Some((.., callbacks, _)) => {
+                let found = callbacks.iter().enumerate().find(|(_, n)| **n == callback);
+                if found.is_none() {
+                    return Err(RemoveCallbackError::NonexistentCallback);
+                }
+                let found = found.unwrap();
+                callbacks.remove(found.0);
+            }
+            None => return Err(RemoveCallbackError::NonexistentCell),
+        }
+        self.callbacks.remove(&callback);
+        Ok(())
+    }
 }
-
-type FontMatrix = [[u8; 3]; 4];
-
-const ZERO: FontMatrix = [
-    [b' ', b'_', b' '],
-    [b'|', b' ', b'|'],
-    [b'|', b'_', b'|'],
-    [b' ', b' ', b' '],
-];
-
-const ONE: FontMatrix = [
-    [b' ', b' ', b' '],
-    [b' ', b' ', b'|'],
-    [b' ', b' ', b'|'],
-    [b' ', b' ', b' '],
-];
-
-const TWO: FontMatrix = [
-    [b' ', b'_', b' '],
-    [b' ', b'_', b'|'],
-    [b'|', b'_', b' '],
-    [b' ', b' ', b' '],
-];
-
-const THREE: FontMatrix = [
-    [b' ', b'_', b' '],
-    [b' ', b'_', b'|'],
-    [b' ', b'_', b'|'],
-    [b' ', b' ', b' '],
-];
-
-const FOUR: FontMatrix = [
-    [b' ', b' ', b' '],
-    [b'|', b'_', b'|'],
-    [b' ', b' ', b'|'],
-    [b' ', b' ', b' '],
-];
-
-const FIVE: FontMatrix = [
-    [b' ', b'_', b' '],
-    [b'|', b'_', b' '],
-    [b' ', b'_', b'|'],
-    [b' ', b' ', b' '],
-];
-
-const SIX: FontMatrix = [
-    [b' ', b'_', b' '],
-    [b'|', b'_', b' '],
-    [b'|', b'_', b'|'],
-    [b' ', b' ', b' '],
-];
-
-const SEVEN: FontMatrix = [
-    [b' ', b'_', b' '],
-    [b' ', b' ', b'|'],
-    [b' ', b' ', b'|'],
-    [b' ', b' ', b' '],
-];
-
-const EIGHT: FontMatrix = [
-    [b' ', b'_', b' '],
-    [b'|', b'_', b'|'],
-    [b'|', b'_', b'|'],
-    [b' ', b' ', b' '],
-];
-
-const NINE: FontMatrix = [
-    [b' ', b'_', b' '],
-    [b'|', b'_', b'|'],
-    [b' ', b'_', b'|'],
-    [b' ', b' ', b' '],
-];
-
-const FONTS: [(usize, FontMatrix); 10] = [
-    (0, ZERO),
-    (1, ONE),
-    (2, TWO),
-    (3, THREE),
-    (4, FOUR),
-    (5, FIVE),
-    (6, SIX),
-    (7, SEVEN),
-    (8, EIGHT),
-    (9, NINE),
-];
