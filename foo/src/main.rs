d3853de71996197da896466740fa5a416dac95ec
winter.loo <adjkldd@126.com>
2025-03-31 21:24:42 +0800

    update foo

--- foo/src/main.rs
+++ foo/src/main.rs
@@ -1,71 +1,66 @@
-/// A munger which XORs a key with some data
-#[derive(Clone)]
-pub struct Xorcism<Key> {
-    // This field is just to suppress compiler complaints;
-    // feel free to delete it at any point.
-    key: Key,
+// (1, 2), (2, 3), (3, 1), (2, 4), (2, 4)
+// 1,2,3,4
+
+// 1 -> 2, 3
+// 2 -> 3, 1, 4
+// 3 -> 2, 1
+// 4 -> 2
+
+
+//  1 <--> 2 <--> 4
+// /|\    /|\
+//  |      |
+//  |     \|/
+//  +----->3
+use std::collections::HashMap;
+pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
+    let mut graph: HashMap<u8, HashMap<u8, u32>> = HashMap::new();
+
+    for (n1, n2) in input {
+        graph.entry(*n1).and_modify(|m| {
+            m.entry(*n2).and_modify(|i| *i += 1).or_insert(1);
+        }).or_insert(HashMap::from([(*n2, 1u32)]));
+        if n1 != n2 {
+            graph.entry(*n2).and_modify(|m| {
+                m.entry(*n1).and_modify(|i| *i += 1).or_insert(1);
+            }).or_insert(HashMap::from([(*n1, 1u32)]));
+        }
+    }
+
+    println!("graph: {graph:#?}");
+
+    let nodes: Vec<_> = graph.keys().copied().collect();
+
+    for node in nodes {
+        let mut path: Vec<(u8, u8)> = vec![];
+        walk_graph(node, node, &mut graph, &mut path);
+        if !path.is_empty() {
+            return Some(path);
+        }
+    }
+
+    None
 }
 
-impl<Key: AsRef<[u8]>> Xorcism<Key> {
-    /// Create a new Xorcism munger from a key
-    ///
-    /// Should accept anything which has a cheap conversion to a byte slice.
-    pub fn new(key: Key) -> Xorcism<Key> {
-        Self { key }
+fn walk_graph(initial: u8, node: u8, graph: &mut HashMap<u8, HashMap<u8, u32>>, path: &mut Vec<(u8, u8)>) {
+    println!("initial: {initial}, node: {node}, graph: {:?}, path: {:?}", graph, path);
+    if path.len() == graph.len() && initial == node {
+        return;
     }
-
-    /// XOR each byte of the input buffer with a byte from the key.
-    ///
-    /// Note that this is stateful: repeated calls are likely to produce different results,
-    /// even with identical inputs.
-    pub fn munge_in_place(&mut self, data: &mut [u8]) {
-        let key_bytes = self.key.as_ref();
-        let mut extended_key = key_bytes.repeat(data.len() / key_bytes.len());
-        let c = &key_bytes[0..data.len() % key_bytes.len()];
-        extended_key.extend(c);
-
-        extended_key
-            .iter()
-            .zip(data)
-            .for_each(|(key_byte, data_byte)| *data_byte ^= *key_byte);
-    }
-
-    /// XOR each byte of the data with a byte from the key.
-    ///
-    /// Note that this is stateful: repeated calls are likely to produce different results,
-    /// even with identical inputs.
-    ///
-    /// Should accept anything which has a cheap conversion to a byte iterator.
-    /// Shouldn't matter whether the byte iterator's values are owned or borrowed.
-    pub fn munge<Data>(&mut self, data: Data) -> impl Iterator<Item = u8>
-    where
-        Data: AsRef<[u8]>,
-    {
-        let key_bytes = self.key.as_ref();
-
-        struct MyIter<'b, Data> {
-            len: usize,
-            key: &'b [u8],
-            data: Data,
+    // use `Option::cloned()` to avoid the immutable/mutable issue
+    let edges = graph.get(&node).cloned().unwrap_or_default();
+    for (adjacent_node, count) in edges {
+        if count == 0 {
+            continue;
         }
+        *graph.get_mut(&node).unwrap().get_mut(&adjacent_node).unwrap() -= 1;
+        *graph.get_mut(&adjacent_node).unwrap().get_mut(&node).unwrap() -= 1;
 
-        impl<Data: AsRef<[u8]>> Iterator for MyIter<'_, Data> {
-            type Item = u8;
+        path.push((node, adjacent_node));
+        walk_graph(initial, adjacent_node, graph, path);
+        path.pop();
 
-            fn next(&mut self) -> Option<Self::Item> {
-                if self.len >= self.data.as_ref().len() {
-                    None
-                } else {
-                    self.len += 1;
-                    Some(self.key[self.len - 1] ^ self.data.as_ref()[self.len - 1])
-                }
-            }
-        }
-
-        MyIter {
-            len: 0,
-            key: key_bytes,
-            data,
-        }
+        *graph.get_mut(&node).unwrap().get_mut(&adjacent_node).unwrap() += 1;
+        *graph.get_mut(&adjacent_node).unwrap().get_mut(&node).unwrap() += 1;
     }
 }
