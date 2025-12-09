5f185263adf6fc19486ab71d47b51b66cf5ddd67
winter.loo <winterloo@126.com>
2025-12-09 15:26:39 +0800

    use thread to replace a waker
    In this demo, every task starts an internal thread and we notify the
    runloop in the internal thread. No need a waker!

--- async-runtime/src/main.rs
+++ async-runtime/src/main.rs
@@ -1,8 +1,9 @@
-use std::sync::{mpsc, Arc};
-use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
+use std::sync::{Arc, Mutex, mpsc};
+use std::task::Poll;
 use std::time::Duration;
 
-type Task = std::pin::Pin<Box<dyn Future<Output = u64>>>;
+// Send + Sync are added as the task will be sent back and forth through thread
+type Task = std::pin::Pin<Box<dyn Future<Output = u64> + Send + Sync>>;
 struct RunLoop {
     rx: mpsc::Receiver<Task>,
     tx: mpsc::SyncSender<Task>,
@@ -15,7 +16,7 @@ struct Spawner {
 impl Spawner {
     fn spawn<F>(&self, fut: F)
     where
-        F: Future<Output = u64> + 'static,
+        F: Future<Output = u64> + 'static + Send + Sync,
     {
         let x = Box::into_pin(Box::new(fut));
         let _ = self.tx.try_send(x);
@@ -37,17 +38,7 @@ impl RunLoop {
             match self.rx.recv() {
                 Ok(mut task) => {
                     println!("received a task");
-                    // Create a Spawner from the current runloop's tx.
-                    // This Spawner will be given to the Waker.
-                    let spawner_instance = Spawner {
-                        tx: self.tx.clone(),
-                    };
-
-                    let waker = unsafe {
-                        Waker::new(&spawner_instance as *const Spawner as *const (), &VTABLE)
-                    };
-                    let mut ctx = Context::from_waker(&waker);
-
+                    let mut ctx = std::task::Context::from_waker(std::task::Waker::noop());
                     match task.as_mut().poll(&mut ctx) {
                         Poll::Ready(_) => {
                             println!("task ready");
@@ -68,22 +59,35 @@ impl RunLoop {
 
 struct MyTask {
     // use reference counting to avoid copy data
-    inner: Arc<Inner>,
+    inner: Arc<Mutex<Inner>>,
 }
 
 struct Inner {
-    sleep_time: std::time::Duration,
-    start_time: std::time::Instant,
+    ready: bool,
 }
 
 impl MyTask {
-    fn new(sleep_time: std::time::Duration) -> Self {
-        Self {
-            inner: Arc::new(Inner {
-                sleep_time,
-                start_time: std::time::Instant::now(),
-            }),
-        }
+    fn new(sleep_time: std::time::Duration, spawner: &Spawner) -> Self {
+        let t = MyTask {
+            inner: Arc::new(Mutex::new(Inner { ready: false })),
+        };
+
+        let inner = Arc::clone(&t.inner);
+        let tx = spawner.tx.clone();
+        // start a thread to do the hard work
+        // and signal the runloop when work done
+        let _jh = std::thread::spawn(move || {
+            std::thread::sleep(sleep_time);
+            {
+                let mut inner = inner.lock().unwrap();
+                inner.ready = true;
+            }
+            // future(task) sent in this thread and received in the main thread
+            let _ = tx.try_send(Box::pin(Box::new(MyTask {
+                inner: Arc::clone(&inner),
+            })));
+        });
+        t
     }
 }
 
@@ -92,68 +96,22 @@ impl Future for MyTask {
 
     fn poll(
         self: std::pin::Pin<&mut Self>,
-        ctx: &mut std::task::Context<'_>,
+        _ctx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Self::Output> {
-        if std::time::Instant::now() - self.inner.start_time < self.inner.sleep_time {
-            //
-            // We need somehow push the task into runloop
-            //
-            // And here, we pass into the task spawner as the any data field and use it
-            // to send back the task
-            //
-            // lightweight task copy
-            let new_task = MyTask {
-                inner: Arc::clone(&self.inner),
-            };
-            let spawner = unsafe { &*(ctx.waker().data() as *const Spawner) };
-            spawner.spawn(new_task);
+        let inner = self.inner.lock().unwrap();
+        if !inner.ready {
+            // move task signaling to the thread created in the constructor
             Poll::Pending
         } else {
-            std::task::Poll::Ready((std::time::Instant::now() - self.inner.start_time).as_secs())
+            std::task::Poll::Ready(std::time::Instant::now().elapsed().as_secs())
         }
     }
 }
 
-// Helper function to get `Arc<Spawner>` from `*const ()`
-unsafe fn data_to_arc_spawner(data: *const ()) -> Spawner {
-    let s = unsafe { &*(data as *const Spawner) };
-    Spawner { tx: s.tx.clone() }
-}
-
-fn clone_arc_spawner(data: *const ()) -> RawWaker {
-    let spawner = unsafe { data_to_arc_spawner(data) };
-    RawWaker::new(&spawner as *const Spawner as *const (), &VTABLE)
-}
-
-fn wake_arc_spawner(data: *const ()) {
-    println!("Wake called!");
-    unsafe {
-        let _ = data_to_arc_spawner(data); // Drop the Arc, decrement ref count
-    }
-}
-
-fn wake_by_ref_arc_spawner(data: *const ()) {
-    println!("Wake by ref called!");
-}
-
-fn drop_arc_spawner(data: *const ()) {
-    println!("Drop called!");
-    unsafe {
-        let _ = data_to_arc_spawner(data); // Drop the Arc, decrement ref count
-    }
-}
-
-const VTABLE: RawWakerVTable = RawWakerVTable::new(
-    clone_arc_spawner,
-    wake_arc_spawner,
-    wake_by_ref_arc_spawner,
-    drop_arc_spawner,
-);
-
 fn main() {
     let mut runloop = RunLoop::new(20);
     let spawner = runloop.spawner();
-    let t = MyTask::new(Duration::from_secs(1));
+    let t = MyTask::new(Duration::from_secs(1), &spawner);
 
     spawner.spawn(t);
 
