5c11fb434c6d039de3db54a58cb02347d875d1f5
winter.loo <winterloo@126.com>
2025-12-31 12:53:50 +0800

    hygienic macro

--- macro/src/main.rs
+++ macro/src/main.rs
@@ -1,3 +1,4 @@
+use std::io::{self, Write};
 // [The Little Book of Rust Macros](https://lukaswirth.dev/tlborm/introduction.html)
 macro_rules! four {
     ($b:block) => {
@@ -33,6 +34,18 @@ macro_rules! four {
     };
 }
 
+// see https://lukaswirth.dev/tlborm/decl-macros/minutiae/hygiene.html
+// https://en.wikipedia.org/wiki/Hygienic_macro
+macro_rules! scope_var {
+    ($a:ident) => {
+        let mut $a = $a;
+        $a = 3;
+    };
+    () => {
+        let a = 3;
+    };
+}
+
 fn main() {
     four!();
     four!(fn);
@@ -41,4 +54,11 @@ fn main() {
     four!(2, 3);
     four!(2   ,    3);
     four!(2, 3, 4 5);
+
+    // see https://lukaswirth.dev/tlborm/decl-macros/minutiae/hygiene.html
+    let a = 2;
+    scope_var!();
+    println!("a={a}");
+    scope_var!(a);
+    println!("a={a}");
 }
